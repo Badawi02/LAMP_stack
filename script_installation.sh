8842ff4a0fd42ab16d29179535a814627efaa8e6
Ahmed Badawi <96023421+Badawi02@users.noreply.github.com>
2023-07-09 14:14:07 +0300

    Update script_installation.sh

--- script_installation.sh
+++ script_installation.sh
@@ -8,6 +8,6 @@ sudo php -v
 sudo apt install ufw
 sudo ufw status
 sudo ufw allow in "Apache Full"
-sudo cp index.thml /var/www/html/index.html
+sudo cp index.html /var/www/html/index.html
 sudo service apache2 restart
 sudo service mysql start
